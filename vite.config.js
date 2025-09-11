0f33d17c0a53f4109c6d6e56c369c6eaaf1a3bc9
Anika Kamleshwaran <akam0020@student.monash.edu>
2025-09-11 13:41:59 +1000

    Updated vite.config.js attempt #2

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools()],
-  base: '/FYP_2025/',
+  base: './',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
